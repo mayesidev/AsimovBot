c8cf19f6298adc9056a2efcb0870c2af876792c2
Isaac Mayes <github@mayesid.com>
2019-08-21 20:32:21 -0500

    Create config
    * Create config.json
    * Change bot to pull prefix and login token from config.

--- asimovbot.js
+++ asimovbot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "$"; 
+const config = require("./config.json");
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -8,11 +8,11 @@ client.on("ready", () => {
  
 client.on("message", (message) => {
   // Don't respond to messages without the prefix or from other bots
-  if(!message.content.startsWith(prefix) || message.author.bot) return;
+  if(!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-  if (message.content === (prefix + "ping")) {
+  if (message.content === (config.prefix + "ping")) {
     message.channel.send("pong!");
   }
 });
  
-client.login("SuperSecretBotTokenHere");
+client.login(config.token);
