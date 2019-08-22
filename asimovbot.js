d7fb48e48eb54fa7b07884580720eba7504d777d
Isaac Mayes <github@mayesid.com>
2019-08-21 20:19:50 -0500

    Update asimovbot.js
    * Add a message prefix to check for.
    * Add check if author is a bot.
    * Change to only respond to exact command.

--- asimovbot.js
+++ asimovbot.js
@@ -1,12 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
- 
+const prefix = "$"; 
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
  
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
+  // Don't respond to messages without the prefix or from other bots
+  if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content === (prefix + "ping")) {
     message.channel.send("pong!");
   }
 });
