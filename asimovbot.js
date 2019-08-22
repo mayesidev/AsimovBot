7e3aa8e3d8f3eef4145d9f95a1d35ab67aadb9f6
Isaac Mayes <github@mayesid.com>
2019-08-21 21:03:39 -0500

    Format asimovbot.js

--- asimovbot.js
+++ asimovbot.js
@@ -3,28 +3,27 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
-  console.log("I am ready!");
+    console.log("I am ready!");
 });
- 
+
 client.on("message", (message) => {
-  // Don't respond to messages without the prefix or from other bots
-  if(message.content.indexOf(config.prefix) !== 0 || message.author.bot) return;
+    // Don't respond to messages without the prefix or from other bots
+    if (message.content.indexOf(config.prefix) !== 0 || message.author.bot) return;
 
-  const args = message.content.slice(config.prefix.length).trim().split(/s+/g);
-  const command = args.shift().toLowerCase();
+    const args = message.content.slice(config.prefix.length).trim().split(/s+/g);
+    const command = args.shift().toLowerCase();
 
-  switch(command)
-  {
-    case "ping":
-        message.channel.send("pong!");
-      break;
-    case "pong":
-        message.channel.send("hey, that's my line!");
-      break;
-    // Don't respond to unrecognized commands.
-    default:
-      return;
-  }
+    switch (command) {
+        case "ping":
+            message.channel.send("pong!");
+            break;
+        case "pong":
+            message.channel.send("hey, that's my line!");
+            break;
+        // Don't respond to unrecognized commands.
+        default:
+            return;
+    }
 });
- 
+
 client.login(config.token);
